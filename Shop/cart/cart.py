e186a9243fbc06b5961149475e7daa07b2ac4113
SweetKitt <Faite@bk.ru>
2022-11-18 22:22:32 +0300

    На сайте появилась система купонов

--- Shop/cart/cart.py
+++ Shop/cart/cart.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from main.models import Product
+from coupons.models import Coupon
 
 
 class Cart(object):
@@ -12,7 +13,8 @@ class Cart(object):
             # сохраняем в сессии пустую корзину
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
-
+        # Сохраняем купон.
+        self.coupon_id = self.session.get('coupon_id')
     def add(self, product, quantity=1, update_quantity=False):
         """ Добавление товара в корзину или обновление его количества. """
         product_id = str(product.id)
@@ -63,3 +65,17 @@ class Cart(object):
         # Очистка корзины
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            return Coupon.objects.get(id=self.coupon_id)
+        return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal('100')) * self.get_total_price()
+        return Decimal('0')
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
